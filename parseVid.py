130381fb7a5fb8e02aab83bd4d185f8d81ed5bab
Andras Fekete <bandi13@users.noreply.github.com>
2024-03-16 23:25:38 -0400

    Optimize: thanks ChatGPT!

--- parseVid.py
+++ parseVid.py
@@ -16,25 +16,27 @@ ROW_PER_PACKET=10
 DELAY_PER_PIXEL=0.0003
 
 def sendPanel(sock, ip, port, p):
-#    print("type={}, shape={}, len={}".format(type(p),p.shape,len(p)))
-#    os.system('cls' if os.name == 'nt' else 'clear')
-    for row in range(ceil(HEIGHT / ROW_PER_PACKET)):
-        m = []
-        m.append(4) # 1- WARLS, 2-DRGB, 3-DRGBW, 4-DNRGB
-        m.append(1) # Seconds to wait before return to normal (255-no timeout)
-        startRow = row * ROW_PER_PACKET
-        iOffset = startRow * WIDTH
-        m.append(int(iOffset / 256))  # High-Byte Index of pixel to change
-        m.append(int(iOffset % 256))  # Low-Byte Index of pixel to change
-#        print("{0:3},{1:3}={2:5}:".format(m[2],m[3],i_offset), end='')
-        for n in range(min(ROW_PER_PACKET,HEIGHT - row * ROW_PER_PACKET)):
-            for col in range(WIDTH):
-                m.append(p[startRow+n,col,2])  # Pixel blue value
-                m.append(p[startRow+n,col,1])  # Pixel green value
-                m.append(p[startRow+n,col,0])  # Pixel red value
-#                print(" {0:5},{1:5},{2:5}".format(p[row*ROW_PER_PACKET+n,col,0],p[row*ROW_PER_PACKET+n,col,1],p[row*ROW_PER_PACKET+n,col,2]), end='')
+    packet_size = min(ROW_PER_PACKET, HEIGHT)
+    num_packets = (HEIGHT + ROW_PER_PACKET - 1) // ROW_PER_PACKET
+
+    for packet_index in range(num_packets):
+        start_row = packet_index * ROW_PER_PACKET
+        end_row = min(start_row + ROW_PER_PACKET, HEIGHT)
+        
+        # Packet format
+        # byte0: 1- WARLS, 2-DRGB, 3-DRGBW, 4-DNRGB
+        # byte1: Seconds to wait before return to normal (255-no timeout)
+        # byte2: High-Byte Index of pixel to change
+        # byte3: Low-Byte Index of pixel to change
+        # byte4+n*3: Red value
+        # byte5+n*3: Green value
+        # byte6+n*3: Blue value
+        m = [4, 1, start_row * WIDTH // 256, start_row * WIDTH % 256]
+        
+        for row in range(start_row, end_row):
+            m.extend(p[row, :, 2::-1].flatten())
+
         m = bytes(m)
-#        print("({})".format(len(m)))
         sock.sendto(m, (ip, port))
         time.sleep(DELAY_PER_PIXEL * WIDTH)
 
